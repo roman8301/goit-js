e0ce61cd75dcd7ba436b896577364cad77679f57
roman8301 <roman8301@gmail.com>
2020-07-16 00:04:19 +0300

    Task-5 complited

--- hw-2/js/task-5.js
+++ hw-2/js/task-5.js
@@ -3,5 +3,14 @@
 const buttonTask5Ref = document.querySelector('#task5');
 
 buttonTask5Ref.addEventListener('click', () => {
-  //
+  const checkForSpam = message =>
+    message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+
+  console.log(checkForSpam('Latest technology news'));
+  alert('Следующая проверка!');
+  console.log(checkForSpam('JavaScript weekly newsletter'));
+  alert('Следующая проверка!');
+  console.log(checkForSpam('Get best sale offers now!'));
+  alert('Следующая проверка!');
+  console.log(checkForSpam('[SPAM] How to earn fast money?'));
 });
