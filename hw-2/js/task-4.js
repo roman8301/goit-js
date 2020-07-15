197b51f8dc0f360614c3be1ea6e38982ba6ab1f1
roman8301 <roman8301@gmail.com>
2020-07-15 22:19:24 +0300

    Task-4 complited

--- hw-2/js/task-4.js
+++ hw-2/js/task-4.js
@@ -3,5 +3,21 @@
 const buttonTask4Ref = document.querySelector('#task4');
 
 buttonTask4Ref.addEventListener('click', () => {
-  //
+  const formatString = string => {
+    const stringLengthMax = 40;
+    if (string.length > stringLengthMax) {
+      return string.slice(0, stringLengthMax) + '...';
+    } else {
+      return string;
+    }
+  };
+
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  alert('Оригинальная строка.\nСледующая проверка!');
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  alert('Строка обрезана.\nСледующая проверка!');
+  console.log(formatString('Curabitur ligula sapien.'));
+  alert('Оригинальная строка.\nСледующая проверка!');
+  console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+  alert('Строка обрезана.');
 });
