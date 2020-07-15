4b1ec481998cdbba949f9e5f8e6a1b38f72e72b2
roman8301 <roman8301@gmail.com>
2020-07-16 00:34:58 +0300

    Task-6 complited

--- hw-2/js/task-6.js
+++ hw-2/js/task-6.js
@@ -3,5 +3,32 @@
 const buttonTask6Ref = document.querySelector('#task6');
 
 buttonTask6Ref.addEventListener('click', () => {
-  //
+  let input;
+  const numbers = [];
+  let total = 0;
+
+  do {
+    input = prompt('Введите число');
+
+    if (input === null) {
+      console.log('Отменено пользователем');
+      break;
+    }
+
+    input = Number(input);
+    const notANumber = Number.isNaN(input);
+
+    if (notANumber) {
+      console.log('Было введено не число, попробуйте еще раз');
+      continue;
+    }
+
+    numbers.push(input);
+  } while (true);
+
+  for (const number of numbers) {
+    total += number;
+  }
+
+  console.log(`Общая сумма чисел равна ${total}`);
 });
